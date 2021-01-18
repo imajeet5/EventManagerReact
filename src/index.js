ca6a32a2819b3362832505184f56bcd2863dc9d5
Ajeet Singh <imajeet5@gmail.com>
2021-01-18 22:47:11 +0530

    changes folder structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css'
+import App from './app/layout/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// const rootEl = document.getElementById('root');
+
+// function render() {
+//   ReactDOM.render(<App />, rootEl);
+// }
+// if (module.hot) {
+//   module.hot.accept('./app/layout/App', function () {
+//     setTimeout(render);
+//   });
+// }
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+// render();
